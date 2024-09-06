4b54e3ec513c7e26004dca835f5328d5ed47216c
Your Name <you@example.com>
2024-09-06 17:04:20 -0600

    5:04

--- index.php
+++ index.php
@@ -206,7 +206,7 @@
 
             </div>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Telefono Codigo Inventario</label>
+                <label for="exampleInputEmail1" class="form-label ">Telefono Codigo Inventario</label>
                 <input type="text" class="form-control" name="telefono codigo inventario">
 
             </div>
@@ -218,7 +218,7 @@
 
     <div class="col-8 p-4">
         <table class="table">
-            <thead>
+            <thead class="bg-info">
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">First</th>
@@ -239,18 +239,14 @@
                     <td>Thornton</td>
                     <td>@fat</td>
                 </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                
             </tbody>
         </table>
     </div>
 
     </div>
 
-
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
