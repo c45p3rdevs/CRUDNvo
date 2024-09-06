4053314a559b2d9ecf27c1a92cbd89ac94d97fa2
Your Name <you@example.com>
2024-09-06 14:22:39 -0600

    2:22

--- index.php
+++ index.php
@@ -1,223 +1,258 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    
-<h1 class="text-center p-3">Registro Mantenimiento DGSP</h1>
-<div class="container-fluid row">
-<form class="col-4">
+
+    <h1 class="text-center p-3">Registro Mantenimiento DGSP</h1>
+    <div class="container-fluid row">
+        <form class="col-4 p-3">
+            <h3 class="text-center text-secondary">Registro de Equipos</h3>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">No Control</label>
+                <input type="text" class="form-control" name="No Control">
+
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Responsable Mantenimiento</label>
+                <input type="text" class="form-control" name="Responsable de Manteniemiento">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombre de Usuario</label>
+                <input type="text" class="form-control" name="Nombre de usuario">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Fecha de Manteniemiento</label>
+                <input type="date" class="form-control" name="Fecha de Mantenimiento">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Hora</label>
+                <input type="time" class="form-control" name="hora">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Resguardante</label>
+                <input type="text" class="form-control" name="resguardante">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Departamento</label>
+                <input type="text" class="form-control" name="departamento">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Usuario Pc</label>
+                <input type="text" class="form-control" name="usuario pc">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombre Equipo</label>
+                <input type="text" class="form-control" name="nombre equipo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">IP</label>
+                <input type="text" class="form-control" name="ip">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Sistema Operativo</label>
+                <input type="text" class="form-control" name="sistema operativo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Memoria RAM</label>
+                <input type="text" class="form-control" name="memoria ram">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">CPU MARCA</label>
+                <input type="text" class="form-control" name="cpu marca">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">CPU MODELO</label>
+                <input type="text" class="form-control" name="cpu modelo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">CPU SERIE</label>
+                <input type="text" class="form-control" name="cpu serie">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">CPU Codigo Inventario</label>
+                <input type="text" class="form-control" name="cpu codigo inventario">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Observaciones</label>
+                <input type="text" class="form-control" name="observaciones">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Monitor Marca</label>
+                <input type="text" class="form-control" name="monitor marca">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Monitor Modelo</label>
+                <input type="text" class="form-control" name="moitor modelo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Monitor serie</label>
+                <input type="text" class="form-control" name="monitor serie">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Monitor Codigo Inventario</label>
+                <input type="text" class="form-control" name="monitor codigo inventario">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">UPS MARCA</label>
+                <input type="text" class="form-control" name="ups marca">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">UPS Modelo</label>
+                <input type="text" class="form-control" name="ups modelo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">UPS Codigo Inventario</label>
+                <input type="text" class="form-control" name="ups codigo inventario">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Impresora Marca</label>
+                <input type="text" class="form-control" name="impresora marca">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Impresora IP</label>
+                <input type="text" class="form-control" name="impresora ip">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Impresora Modelo</label>
+                <input type="text" class="form-control" name="impresora modelo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Impresora Serie</label>
+                <input type="text" class="form-control" name="impresora serie">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Impresora Codigo Inventario</label>
+                <input type="text" class="form-control" name="impresora codigo inventario">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Escaner Marca</label>
+                <input type="text" class="form-control" name="escaner marca">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Escaner Modelo</label>
+                <input type="text" class="form-control" name="escaner modelo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Escaner Modelo</label>
+                <input type="text" class="form-control" name="escaner modelo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Escaner Serie</label>
+                <input type="text" class="form-control" name="escaner serie">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Escaner Codigo Inventario</label>
+                <input type="text" class="form-control" name="escaner codigo inventario">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Telefono Marca</label>
+                <input type="text" class="form-control" name="telefono marca">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Telefono IP</label>
+                <input type="text" class="form-control" name="telefono ip">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Telefono Modelo</label>
+                <input type="text" class="form-control" name="telefono modelo">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Telefono Serie</label>
+                <input type="text" class="form-control" name="telefono serie">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Telefono Codigo Inventario</label>
+                <input type="text" class="form-control" name="telefono codigo inventario">
+
+            </div>
 
 
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">No Control</label>
-    <input type="text" class="form-control"  name="No Control">
-   
-  </div>
+    </div>
+    <button type="submit" class="btn btn-primary" name="btnregistrar" value="ok">Registrar</button>
+    </form>
 
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Responsable Mantenimiento</label>
-    <input type="text" class="form-control"  name="Responsable de Manteniemiento">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Nombre de Usuario</label>
-    <input type="text" class="form-control"  name="Nombre de usuario">
-   
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Fecha de Manteniemiento</label>
-    <input type="date" class="form-control"  name="Fecha de Mantenimiento">
-   
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Hora</label>
-    <input type="time" class="form-control"  name="hora">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Resguardante</label>
-    <input type="text" class="form-control"  name="resguardante">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Departamento</label>
-    <input type="text" class="form-control"  name="departamento">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Usuario Pc</label>
-    <input type="text" class="form-control"  name="usuario pc">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Nombre Equipo</label>
-    <input type="text" class="form-control"  name="nombre equipo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">IP</label>
-    <input type="text" class="form-control"  name="ip">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Sistema Operativo</label>
-    <input type="text" class="form-control"  name="sistema operativo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Memoria RAM</label>
-    <input type="text" class="form-control"  name="memoria ram">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">CPU MARCA</label>
-    <input type="text" class="form-control"  name="cpu marca">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">CPU MODELO</label>
-    <input type="text" class="form-control"  name="cpu modelo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">CPU SERIE</label>
-    <input type="text" class="form-control"  name="cpu serie">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">CPU Codigo Inventario</label>
-    <input type="text" class="form-control"  name="cpu codigo inventario">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Observaciones</label>
-    <input type="text" class="form-control"  name="observaciones">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Monitor Marca</label>
-    <input type="text" class="form-control"  name="monitor marca">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Monitor Modelo</label>
-    <input type="text" class="form-control"  name="moitor modelo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Monitor serie</label>
-    <input type="text" class="form-control"  name="monitor serie">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Monitor Codigo Inventario</label>
-    <input type="text" class="form-control"  name="monitor codigo inventario">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">UPS MARCA</label>
-    <input type="text" class="form-control"  name="ups marca">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">UPS Modelo</label>
-    <input type="text" class="form-control"  name="ups modelo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">UPS Codigo Inventario</label>
-    <input type="text" class="form-control"  name="ups codigo inventario">
+    <div class="col-8 p-4">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Impresora Marca</label>
-    <input type="text" class="form-control"  name="impresora marca">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Impresora IP</label>
-    <input type="text" class="form-control"  name="impresora ip">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Impresora Modelo</label>
-    <input type="text" class="form-control"  name="impresora modelo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Impresora Serie</label>
-    <input type="text" class="form-control"  name="impresora serie">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Impresora Codigo Inventario</label>
-    <input type="text" class="form-control"  name="impresora codigo inventario">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Escaner Marca</label>
-    <input type="text" class="form-control"  name="escaner marca">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Escaner Modelo</label>
-    <input type="text" class="form-control"  name="escaner modelo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Escaner Modelo</label>
-    <input type="text" class="form-control"  name="escaner modelo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Escaner Serie</label>
-    <input type="text" class="form-control"  name="escaner serie">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Escaner Codigo Inventario</label>
-    <input type="text" class="form-control"  name="escaner codigo inventario">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Telefono Marca</label>
-    <input type="text" class="form-control"  name="telefono marca">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Telefono IP</label>
-    <input type="text" class="form-control"  name="telefono ip">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Telefono Modelo</label>
-    <input type="text" class="form-control"  name="telefono modelo">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Telefono Serie</label>
-    <input type="text" class="form-control"  name="telefono serie">
-    
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Telefono Codigo Inventario</label>
-    <input type="text" class="form-control"  name="telefono codigo inventario">
-    
-  </div>
-
- 
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
+    </div>
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>l
